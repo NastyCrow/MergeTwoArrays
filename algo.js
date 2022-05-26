ab8bcf643b1fe6b931a93244698ea4fda5ce50aa
Hattan Althobaiti <mr.hattanhassan@gmail.com>
2022-05-26 10:29:02 +0300

    Merge two arrays

--- algo.js
+++ algo.js
@@ -6,9 +6,11 @@
     const expectedOutput = ["a", "b", "c", "c", "e", "f"] 
 */
 
-
-
 const arr1 = ["a", "b", "c"];
 const arr2 = ["c", "e", "f"];
 
-function mergeArray() {}
+function mergeArray(arr1, arr2) {
+  arr1.push(...arr2);
+  return arr1;
+}
+console.log(mergeArray(arr1, arr2));
