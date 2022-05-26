454dfa3be17e5775666a88d4d6f79fa62559a4e4
Hattan Althobaiti <mr.hattanhassan@gmail.com>
2022-05-26 10:13:46 +0300

    initial Commit

---
+++ algo.js
@@ -0,0 +1,14 @@
+/*
+    Given an array arr1 and array arr2, merge both arrays and return one array
+    const arr1 = ["a", "b", "c"]
+    const arr2 = ["c", "e", "f"]
+    
+    const expectedOutput = ["a", "b", "c", "c", "e", "f"] 
+*/
+
+
+
+const arr1 = ["a", "b", "c"];
+const arr2 = ["c", "e", "f"];
+
+function mergeArray() {}
